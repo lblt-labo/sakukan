c5e883a8b0511adbd0b47865fdee27409a78f4f8
lblt-labo <lbltlaboapp@gmail.com>
2026-09-02 21:39:30 +0900

    Add files via upload

---
+++  sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // 最小構成のネットワーク優先フェッチ
+});
